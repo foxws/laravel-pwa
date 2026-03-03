c5e16c84255afbf8f3458946def17df562448800
francoism90 <francoism90@users.noreply.github.com>
2026-03-03 16:59:37 +0100

    Remove head.blade.php view file

--- resources/views/head.blade.php
+++
@@ -1,9 +0,0 @@
-@props([
-    'themeColor' => config('pwa.manifest.theme_color', '#000000'),
-    'icon'       => config('pwa.manifest.icons.0.src', 'logo.png'),
-    'manifest'   => '/manifest.json',
-])
-
-<meta name="theme-color" content="{{ $themeColor }}">
-<link rel="apple-touch-icon" href="{{ asset($icon) }}">
-<link rel="manifest" href="{{ $manifest }}">
