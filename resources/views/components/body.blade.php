0e1899840951ac169244318072ce32a2aaa3530d
francoism90 <francoism90@users.noreply.github.com>
2026-07-06 09:27:22 +0200

    Add update interval

--- resources/views/components/body.blade.php
+++ resources/views/components/body.blade.php
@@ -6,9 +6,8 @@
     'nonce' => Vite::cspNonce(),
     'swPath' => asset(Config::string('pwa.sw_path', 'sw.js')),
     'debug' => Config::boolean('app.debug', false),
+    'updateInterval' => Config::integer('pwa.update_interval', 8),
 ])
-
-<script src="{{ $swPath }}" @isset($nonce) nonce="{{ $nonce }}" @endisset></script>
 
 <script @isset($nonce) nonce="{{ $nonce }}" @endisset>
     "use strict";
@@ -16,10 +15,16 @@
     if ("serviceWorker" in navigator) {
         window.addEventListener("load", () => {
             navigator.serviceWorker
-                .register("{{ $swPath }}", { scope: "{{ $scope }}" })
+                .register("{{ $swPath }}", { scope: "{{ $scope }}", updateViaCache: "none" })
                 .then(
                     (registration) => {
                         @if($debug) console.log("Service worker registration succeeded:", registration); @endif
+                        @if($updateInterval > 0)
+                        setInterval(() => {
+                            registration.update();
+                            @if($debug) console.log("Service worker update check triggered."); @endif
+                        }, {{ (int) $updateInterval * 60 * 60 * 1000 }});
+                        @endif
                     },
                     (error) => {
                         @if($debug) console.error("Service worker registration failed:", error); @endif
